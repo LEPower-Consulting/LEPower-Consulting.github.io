5a5d546ee772b077588465131e1ed68f2b985748
LEPower-Consulting <shermuhammad.qayumov.02@inbox,ru>
2021-10-28 14:40:51 +0500

    all

--- sher1.php
+++ sher1.php
@@ -24,7 +24,7 @@
         <a class="link-secondary" href="#" aria-label="Search">
           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="mx-3" role="img" viewBox="0 0 24 24"><title>Search</title><circle cx="10.5" cy="10.5" r="7.5"></circle><path d="M21 21l-5.2-5.2"></path></svg>
         </a>
-        <a class="btn btn-sm btn-outline-secondary" href="#">Sign up</a>
+        <a class="btn btn-sm btn-outline-secondary" href="Sign-up.php">Sign up</a>
       </div>
     </div>
   </header>
@@ -228,9 +228,9 @@
         <div class="p-4">
           <h4 class="fst-italic">Elsewhere</h4>
           <ol class="list-unstyled">
-            <li><a href="#">GitHub</a></li>
+            <li><a href="https://t.me/KayumovSher">Telegramm</a></li>
             <li><a href="#">Twitter</a></li>
-            <li><a href="#">Facebook</a></li>
+            <li><a href="https://www.facebook.com/ykayumov">Facebook</a></li>
           </ol>
         </div>
       </div>
@@ -287,9 +287,10 @@
             <i class="fas fa-gem me-3"></i>Company name
           </h6>
           <p>
-            Here you can use rows and columns to organize your footer content. Lorem ipsum
-            dolor sit amet, consectetur adipisicing elit.
-          </p>
+            Basically: <br>
+            1.Working with Corporate projects <br>
+            2.State plan managemant <br>
+            3.Investment projects
         </div>
         <!-- Grid column -->
 
@@ -341,13 +342,14 @@
           <h6 class="text-uppercase fw-bold mb-4">
             Contact
           </h6>
-          <p><i class="fas fa-home me-3"></i> New York, NY 10012, US</p>
+          <p><i class="fas fa-home me-3"></i> Tashkent, Bog' 1/105, UZ</p>
           <p>
             <i class="fas fa-envelope me-3"></i>
-            info@example.com
+            shermuhammad.qayumov.02@mail.ru <br>
           </p>
-          <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
-          <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
+          <p><i class="fas fa-phone me-3"></i> +81 90-6472-2081</p>
+          <p><i class="fas fa-print me-3"></i> +998 93-511-11-65</p>
+          <p><i class="fas fa-print me-3"></i> +998 90-397-88-02</p>
         </div>
         <!-- Grid column -->
       </div>
@@ -359,7 +361,8 @@
   <!-- Copyright -->
   <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
     © 2021 Copyright:
-    <a class="text-reset fw-bold" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+    <a class="text-reset fw-bold" href="https://lepower-consulting.github.io/">
+  LEPower-Consulting.github.io</a>
   </div>
   <!-- Copyright -->
 </footer>
